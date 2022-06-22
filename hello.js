3c79c67ed90e8a1580540dc75418ea056feaba4d
David Michel <david.michel2809@gmail.com>
2022-06-22 08:57:13 +0200

    add function sayHello10Times

--- hello.js
+++ hello.js
@@ -3,3 +3,9 @@ console.log("Bonjour ça va les amis ?");
 function sayHello() {
   "Bonjour les amis !";
 }
+
+const sayHello10Times = () => {
+  for (let i = 0; i < 10; i++) {
+    sayHello();
+  }
+};
